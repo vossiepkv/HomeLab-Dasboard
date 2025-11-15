c987d2e6af90e17a76d7618ad739fb3be28b9928
vossiepkv <vossiepkv@gmail.com>
2025-11-15 21:48:32 +1100

    created signin page signup page and integrated firebase for authentication

---
+++ Scripts/FireBaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDVteaGRDL9iR6OMFWD3zfoRNT9t8JJqnI",
+  authDomain: "olympus-76205.firebaseapp.com",
+  projectId: "olympus-76205",
+  storageBucket: "olympus-76205.firebasestorage.app",
+  messagingSenderId: "814227322680",
+  appId: "1:814227322680:web:9db7c6afe7615fe940f3cd"
+};
